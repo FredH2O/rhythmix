4d190ad5e35a8cc41fdfda3104e7b42438e3467e
Fred <huertaserick@yahoo.ie>
2025-08-28 19:47:46 +0100

    feat: scroll velocity for catchy phrase added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 import Features from "@/components/Feature/Feature";
 import Hero from "@/components/Hero/Hero";
+import ScrollVelocity from "@/components/Reactbits/ScrollVelocity";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <ScrollVelocity
+        texts={[
+          "“Turn Up the Night, Book Your DJ Today.”",
+          "“Your Soundtrack, Our DJ.”",
+        ]}
+        velocity={40}
+        className="custom-scroll-text text-slate-500"
+      />
       <Features />
     </div>
   );
